e1478cc8d3448ffa2b22b49be6b743df0be52d49
tajul7472 <tajul7472@gmail.com>
2023-02-24 19:33:26 +0600

    counter app

--- counter/src/components/redux/store.js
+++ counter/src/components/redux/store.js
@@ -2,7 +2,8 @@ import { applyMiddleware, createStore } from 'redux'
 import rootReducer from './rootReducer'
 import logger from 'redux-logger'
 import myLogger from './midddleWare/mylogger'
+import {composeWithDevTools} from "redux-devtools-extension"
 
-const store = createStore(rootReducer, applyMiddleware(logger,myLogger))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger,myLogger)))
 
 export default store
